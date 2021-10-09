b3c6ba9c600ad75418b39b52748863b3fa297968
Quentin18 <quentindeschamps18@orange.fr>
2021-10-09 17:58:57 +0200

    update lab exercise 02

--- labs/ex02/template/stochastic_gradient_descent.py
+++ labs/ex02/template/stochastic_gradient_descent.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """Stochastic Gradient Descent"""
-import costs
-import gradient_descent
-import helpers
+from costs import compute_loss
+from gradient_descent import compute_gradient
+from helpers import batch_iter
 
 
 def compute_stoch_gradient(y, tx, w):
@@ -10,7 +10,7 @@ def compute_stoch_gradient(y, tx, w):
     Compute a stochastic gradient from just few examples n
     and their corresponding y_n labels.
     """
-    return gradient_descent.compute_gradient(y, tx, w)
+    return compute_gradient(y, tx, w)
 
 
 def stochastic_gradient_descent(y, tx, initial_w, batch_size, max_iters, gamma):
@@ -20,14 +20,15 @@ def stochastic_gradient_descent(y, tx, initial_w, batch_size, max_iters, gamma):
     losses = []
     w = initial_w
     for n_iter in range(max_iters):
-        for minibatch_y, minibatch_tx in helpers.batch_iter(y, tx, batch_size):
+        for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size):
             grad = compute_stoch_gradient(minibatch_y, minibatch_tx, w)
-            loss = costs.compute_loss(y, tx, w)
+            loss = compute_loss(y, tx, w)
             w = w - gamma * grad
-            # store w and loss
-            ws.append(w)
-            losses.append(loss)
-            print("Stochastic Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
-                  bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+
+        # store w and loss
+        ws.append(w)
+        losses.append(loss)
+        print("Stochastic Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+                bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
 
     return losses, ws
