ecd19756a7bbbb8d32e5dd4f9ae3d5809f591477
Quentin18 <quentindeschamps18@orange.fr>
2021-10-14 19:44:23 +0200

    fix ex03

--- labs/ex03/template/build_polynomial.py
+++ labs/ex03/template/build_polynomial.py
@@ -5,5 +5,4 @@ import numpy as np
 
 def build_poly(x, degree):
     """polynomial basis functions for input data x, for j=0 up to j=degree."""
-    phi_n = lambda x_n: [x_n**d for d in range(degree + 1)]
-    return np.array(list(map(phi_n, x)))
+    return np.vander(x, degree + 1, increasing=True)
